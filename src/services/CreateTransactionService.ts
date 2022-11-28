662296c8811f9c539c4697c5bbba8f63202c6af8
Marcelo Bianco <marcelognn@gmail.com>
2022-11-27 23:14:10 -0300

    List and Create Transacations

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,45 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+import Transaction from '../models/Transaction';
+import { getCustomRepository, getRepository } from 'typeorm';
+
+
 // import AppError from '../errors/AppError';
 
-import Transaction from '../models/Transaction';
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type, category }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const categoryRepository = getRepository(Category);
+    let checkCategoryExists = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!checkCategoryExists) {
+      checkCategoryExists = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(checkCategoryExists);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: checkCategoryExists,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
